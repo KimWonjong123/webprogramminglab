1c15296a3ef6b6d8d37b3349e3cc41bafcbde821
KimWonjong123 <bonk159@gmail.com>
2023-10-21 21:34:11 +0900

    withdraw success

---
+++ midterm/scripts/withdraw-success.js
@@ -0,0 +1,18 @@
+function init() {
+    let btn = document.querySelector('#home');
+    btn.onclick = () => {
+        window.location.href = `./main.html`;
+    }
+
+    btn = document.querySelector('#again');
+    btn.onclick = () => {
+        window.location.href = `./withdraw.html`;
+    }
+
+    btn = document.querySelector('#return-card');
+    btn.onclick = () => {
+        window.location.href = `../index.html`;
+    }
+}
+
+init();
