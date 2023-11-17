bbac7642c2f7b13e63974e26ad8b06ff42c411c3
KimWonjong123 <bonk159@gmail.com>
2023-11-17 13:14:07 +0900

    tab fadeout fadein

--- Assignment04/script.js
+++ Assignment04/script.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
+    $(".tab-content").hide();
+    $(".tab-content:first").show();
+
     $("ul.tabs li").click(function () {
         var tab_id = $(this).attr("tab");
 
         $("ul.tabs li").removeClass("active");
-        $(".tab-content").removeClass("active");
-
         $(this).addClass("active");
-        $("#" + tab_id).addClass("active");
+        $(".tab-content.active").fadeOut(500, () => {
+            $(".tab-content.active").removeClass("active");
+            $("#" + tab_id).addClass("active").fadeIn(500);
+        });
     });
 });
